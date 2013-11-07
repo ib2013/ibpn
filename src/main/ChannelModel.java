f60bbc4af3b022be2833745a6fbce97d52b143db
Nadin Zajimovic <nadin_zajimovic@hotmail.com>
2013-11-07 15:28:15 +0100

    Promjena

--- src/main/ChannelModel.java
+++ src/main/ChannelModel.java
@@ -3,24 +3,49 @@ package main;
 public class ChannelModel {
 	String name;
 	String description;
-	
+
 	public ChannelModel(String name, String description) {
 		super();
 		this.name = name;
 		this.description = description;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ChannelModel other = (ChannelModel) obj;
+		return name.equals(other.getName());
 	}
 
 }
--- src/main/FeedToPushServlet.java
+++ src/main/FeedToPushServlet.java
@@ -4,8 +4,8 @@ import javax.servlet.http.HttpServlet;
 
 public class FeedToPushServlet extends HttpServlet {
 	public void init(){
-		FeedToPushService app = new FeedToPushService();
-		app.start();
+		FeedToPushService service = new FeedToPushService();
+		service.start();
 	}
 
 }
