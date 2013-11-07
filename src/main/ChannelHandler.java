a86c46307e05bce01fc5e8ef7c8637a8b6bff0db
djovanovic <skr.dejanjovanovic@gmail.com>
2013-11-07 14:38:00 +0100

    Dodat metod updateChannel u ChannelHandler

--- src/main/ChannelHandler.java
+++ src/main/ChannelHandler.java
@@ -2,6 +2,14 @@ package main;
 
 import java.util.ArrayList;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.gson.Gson;
+
 public class ChannelHandler {
 	public ArrayList<ChannelModel> fetchChannelList(){
 		return null;
@@ -13,6 +21,27 @@ public class ChannelHandler {
 	
 	public void deleteChannel(ChannelModel channel){
 		
+	}
+	
+	
+	public void updateChannel(ChannelModel oldModel, ChannelModel newModel) {
+		
+		Gson gson = new Gson();
+		try {
+			StringEntity parms = new StringEntity(gson.toJson(oldModel));
+			
+			HttpClient client = new DefaultHttpClient();
+			HttpPut request = new HttpPut("https://pushapi.infobip.com/1/application/"
+					+ Configuration.APPLICATION_ID
+					+ "/channel/"
+					+ oldModel.getName());
+			request.addHeader("Authorization", Configuration.AUTHORIZATION_INFO);
+			request.setEntity(parms);
+			HttpResponse response = client.execute(request);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
