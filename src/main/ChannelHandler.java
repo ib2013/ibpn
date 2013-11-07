2413ad95b1b9dcb7955e5f184759cdfeedd42d02
= <=>
2013-11-07 14:33:05 +0100

    haj cao

--- src/main/ChannelHandler.java
+++ src/main/ChannelHandler.java
@@ -2,13 +2,20 @@ package main;
 
 import java.util.ArrayList;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class ChannelHandler {
 	public ArrayList<ChannelModel> fetchChannelList(){
 		return null;
 	}
 	
 	public void addChannel(ChannelModel channel){
-		
+		HttpClient client = new DefaultHttpClient();
+		HttpPost request = new HttpPost(
+				"https://pushapi.infobip.com/1/application/9cabf301d3db/channels");
+		request.addHeader("Authorization", "Basic cHVzaGRlbW86cHVzaGRlbW8=");
 	}
 	
 	public void deleteChannel(ChannelModel channel){
