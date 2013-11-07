2340fbaf88be48dcab1e35de890b2a68d9ad9250
Imer Husejnovic <imer90@gmail.com>
2013-11-07 14:23:23 +0100

    Dodata metoda za brisanje kanala

--- src/main/ChannelHandler.java
+++ src/main/ChannelHandler.java
@@ -2,17 +2,35 @@ package main;
 
 import java.util.ArrayList;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class ChannelHandler {
-	public ArrayList<ChannelModel> fetchChannelList(){
+	public ArrayList<ChannelModel> fetchChannelList() {
 		return null;
 	}
-	
-	public void addChannel(ChannelModel channel){
-		
+
+	public void addChannel(ChannelModel channel) {
+
 	}
-	
-	public void deleteChannel(ChannelModel channel){
-		
+
+	public void deleteChannel(ChannelModel channel) {
+		HttpClient client = new DefaultHttpClient();
+		HttpDelete request = new HttpDelete(
+				"https://pushapi.infobip.com/1/application/"
+						+ main.Configuration.AUTHORIZATION_INFO + "/channels");
+		request.addHeader("Authorization",
+				main.Configuration.AUTHORIZATION_INFO);
+		request.addHeader("applicationID", main.Configuration.APPLICATION_ID);
+		request.addHeader("channelName", channel.getName());
+
+		try {
+			HttpResponse response = client.execute(request);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
 }
