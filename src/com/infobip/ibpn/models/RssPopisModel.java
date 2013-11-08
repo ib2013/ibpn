6ab3b522cee8790b8a9464eba148ea3dec65a849
Nadin Zajimovic <nadin_zajimovic@hotmail.com>
2013-11-08 10:17:22 +0100

    prevodjenje

--- src/com/infobip/ibpn/models/RssPopisModel.java
+++ src/com/infobip/ibpn/models/RssPopisModel.java
@@ -61,5 +61,37 @@ public class RssPopisModel {
 		return "RssPopisModel [id=" + id + ", rssFeed=" + rssFeed + ", opis="
 				+ opis + ", idRssSource=" + idRssSource + "]";
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + idRssSource;
+		result = prime * result + ((opis == null) ? 0 : opis.hashCode());
+		result = prime * result + ((rssFeed == null) ? 0 : rssFeed.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RssPopisModel other = (RssPopisModel) obj;
+		if (id != other.id)
+			return false;
+		if (idRssSource != other.idRssSource)
+			return false;
+		if (rssFeed == null) {
+			if (other.rssFeed != null)
+				return false;
+		} else if (!rssFeed.equals(other.rssFeed))
+			return false;
+		return true;
+	}
 
 }
